da3eb22ee2a40c34729b0591de4695c7fa4cf4c2
adriraj2000 <adrirajc@gmail.com>
2021-02-23 20:29:46 +0530

    getting all questions

--- handler/quiz.js
+++ handler/quiz.js
@@ -4,6 +4,39 @@ const axios = require('axios')
 
 //Open Trivia Database - https://opentdb.com/api.php
 
+function getCategoryID(category) {
+    category = category.toLowerCase();
+    switch (category) {
+        case 'any': return '';
+        case 'general': return 9;
+        case 'books': return 10;
+        case 'film': return 11;
+        case 'music': return 12;
+        case 'theatre': return 13;
+        case 'television': return 14;
+        case 'videogames': return 15;
+        case 'boardgames': return 16;
+        case 'science': return 17;
+        case 'computers': return 18;
+        case 'mathematics':
+        case 'math': return 19;
+        case 'mythology': return 20;
+        case 'sports': return 21;
+        case 'geography': return 22;
+        case 'history': return 23;
+        case 'politics': return 24;
+        case 'art': return 25;
+        case 'celebrities': return 26;
+        case 'animals': return 27;
+        case 'vehicles': return 28;
+        case 'comics': return 29;
+        case 'gadgets': return 30;
+        case 'anime': return 31;
+        case 'cartoons': return 32;
+    }
+}
+
+
 const quizHandler = {
 
     getquestions: async (req, res) => {
@@ -12,24 +45,20 @@ const quizHandler = {
             const testid = req.body.code
             const email = req.body.email
             const doc = await Quiz.find({ code: testid })
-            //console.log(doc)
             if (!doc) {
                 return res.status(400).json({ msg: "This test does not exist." })
             }
-            if (Date.parse(doc.expiry) < Date.now()) {
+            if (Date.parse(doc[0].expiry) < Date.now()) {
                 return res.status(400).send({ message: "Test has expired!! " });
             }
             const check = await Result.findOne({ code: testid, email })
             if (check) {
                 return res.status(400).send({ message: "Test already taken!" });
             }
-            const questions = await axios.get("https://opentdb.com/api.php",{ 
-                params: {
-                    amount: Number(doc.amount),
-                    category: doc.topic,
-                },
-            });
-            
+            const categoryID = getCategoryID(doc[0].topic)
+            const url = `https://opentdb.com/api.php?amount=${doc[0].amount}&category=${categoryID}&difficulty=easy&type=multiple`
+            const questions = await axios.get(url);
+            console.log(questions.data);
         } catch (error) {
             return res.status(500).json({ msg: error.message })
         }
