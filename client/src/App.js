5d7f603229ccb460b12d5b996908cac69bfc07fa
adriraj2000 <adrirajc@gmail.com>
2021-02-26 18:32:47 +0530

    rewrite react

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,9 @@
 import React, { Fragment, useState } from "react";
-import { Route, Switch, useLocation, Redirect } from "react-router-dom";
-import Homenav from "./components/Home";
-import Takequiz from "./components/Takequiz";
-import dashboard from "./components/Dashboard";
+
 
 function App() {
-  const [loggedin, setloggedin] = useState(false);
+  
 
-  let location = useLocation();
-  return (
-    <Fragment>
-      <nav>
-        <Homenav setloggedin={setloggedin} />
-      </nav>
-      <main>
-        <Switch>
-          <Route exact path="/" component={Takequiz} />
-          <Route exact path="/dashboard" component={dashboard} />
-          <Route render={() => <Redirect to="/" />} />
-        </Switch>
-      </main>
-    </Fragment>
-  );
 }
 
 export default App;
